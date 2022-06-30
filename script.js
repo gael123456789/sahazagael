0e44f4ecb4abff2d839b5153b09a1a27337e756d
gael123456789 <108399286+gael123456789@users.noreply.github.com>
2022-06-30 21:09:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ["Hello to all, I introduce myself my name is Gael", " I am a man passionate about computers since my childhood, especially pentesting. After a career that made me discover several professional backgrounds such as network administration, IT support, IT Manager, deployment agent, programmer, pentester. I decided to throw myself fully into learning the code, for nearly 6 years I have been training myself full-time in several professional hacking languages and techniques in online platforms such as try hack me,hack the box, open classroom."],
+    typeSpeed: 20,
+  });
+ 
